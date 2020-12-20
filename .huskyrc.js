1a657dcf6bf613fc78b780ec527abd86db3f41fe
Daniele Belardi <dwon.dnl@gmail.com>
2020-12-20 10:35:21 +0100

    build(deps-dev): remove commitizen

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,7 +1,5 @@
 module.exports = {
   hooks: {
-    'prepare-commit-msg': 'exec < /dev/tty && git cz --hook || true',
-    'pre-commit': 'lint-staged',
-    'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS'
+    'pre-commit': 'lint-staged'
   }
 }
